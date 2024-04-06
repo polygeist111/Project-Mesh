695a893a504568a22e601732c6768f282c61ca7c
gvarvar <george.varvaris@grey-box.ca>
2024-04-06 22:52:28 +1100

    Start refactoring

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://devserver3.ustadmobile.com/maven2/")
+    }
+}
+
+rootProject.name = "Project Mesh"
+include(":app")
+ 

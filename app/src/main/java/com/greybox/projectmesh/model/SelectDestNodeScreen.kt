5c0471cb9cec85d3642bb055ff1b708173ffd0a4
kelvinlin9614 <kelvinlin9614@gmail.com>
2024-10-18 13:32:51 -0700

    add FileProvider in AndroidManifest.xml

--- app/src/main/java/com/greybox/projectmesh/model/SelectDestNodeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.greybox.projectmesh.model
-
-import com.ustadmobile.meshrabiya.vnet.VirtualNode
-
-data class SelectDestNodeScreenModel(
-    val allNodes: Map<Int, VirtualNode.LastOriginatorMessage> = emptyMap(),
-    val uri: String = "",
-    val contactingInProgressDevice: String? = null,
-)

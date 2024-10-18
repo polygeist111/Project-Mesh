d04c67f499f4248d12d653d5bbbb0ce58e23dd46
kelvinlin9614 <kelvinlin9614@gmail.com>
2024-10-18 12:15:46 -0700

    Update InfoScreen.kt
    add info

--- app/src/main/java/com/greybox/projectmesh/views/InfoScreen.kt
+++ app/src/main/java/com/greybox/projectmesh/views/InfoScreen.kt
@@ -9,24 +9,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun InfoScreen() {
-    Column (modifier = Modifier.fillMaxSize().padding(18.dp)){
-        Text("Project Mesh -- Version 1.0")
-        Spacer(modifier = Modifier.height(10.dp))
-        Text("Device Name: ${Build.MANUFACTURER} ${Build.MODEL}")
-        Spacer(modifier = Modifier.height(10.dp))
-        Text("Android Version: ${Build.VERSION.RELEASE}")
-        Spacer(modifier = Modifier.height(10.dp))
-        Text("SDK Version: ${Build.VERSION.SDK_INT}")
-        Spacer(modifier = Modifier.height(10.dp))
+    Column (modifier = Modifier.fillMaxSize().padding(20.dp)){
+        Text("Project Mesh -- Version 1.0", style = TextStyle(fontSize = 18.sp))
+        Spacer(modifier = Modifier.height(12.dp))
+        Text("Device Name: ${Build.BRAND.substring(0, 1).uppercase() + Build.BRAND.substring(1).lowercase()} ${Build.MODEL}",
+            style = TextStyle(fontSize = 18.sp))
+        Spacer(modifier = Modifier.height(12.dp))
+        Text("Android Version: ${Build.VERSION.RELEASE}", style = TextStyle(fontSize = 18.sp))
+        Spacer(modifier = Modifier.height(12.dp))
+        Text("SDK Version: ${Build.VERSION.SDK_INT}", style = TextStyle(fontSize = 18.sp))
+        Spacer(modifier = Modifier.height(12.dp))
     }
 
 }

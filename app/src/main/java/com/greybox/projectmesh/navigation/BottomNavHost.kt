4c42701d810cbfaa317ff5af87bc725218a60bd3
Bruno Silva-Cubillos <brunosilvacubillos@outlook.fr>
2025-06-09 17:06:06 -0400

    Nav-Bar Modification
    Updated the navigation bar to be able to hold all the title of the icons to make it look better. While also adding a Preview so that we can see what is being changed in the backend of the BottomNavHost.

--- app/src/main/java/com/greybox/projectmesh/navigation/BottomNavHost.kt
+++ app/src/main/java/com/greybox/projectmesh/navigation/BottomNavHost.kt
@@ -6,6 +6,11 @@ import androidx.navigation.NavHostController
 import androidx.compose.material3.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
 import com.greybox.projectmesh.R
 
 data class NavigationItem(
@@ -14,6 +19,14 @@ data class NavigationItem(
     val icon: ImageVector,
 )
 
+//Preview is to show the bottom navigation bar in the preview and notice what it looks like
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationBarPreview() {
+    val navController = rememberNavController()
+    BottomNavigationBar(navController = navController)
+}
+
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
     val items = listOf(
@@ -21,16 +34,25 @@ fun BottomNavigationBar(navController: NavHostController) {
         NavigationItem(BottomNavItem.Network.route, stringResource(id = R.string.network), BottomNavItem.Network.icon),
         NavigationItem(BottomNavItem.Send.route, stringResource(id = R.string.send), BottomNavItem.Send.icon),
         NavigationItem(BottomNavItem.Receive.route, stringResource(id = R.string.receive), BottomNavItem.Receive.icon),
+        NavigationItem(BottomNavItem.Chat.route, stringResource(id=R.string.chat), BottomNavItem.Chat.icon),
         NavigationItem(BottomNavItem.Log.route, stringResource(id = R.string.log), BottomNavItem.Log.icon),
-        NavigationItem(BottomNavItem.Settings.route, stringResource(id = R.string.settings), BottomNavItem.Settings.icon),
-        NavigationItem(BottomNavItem.Chat.route, stringResource(id=R.string.chat), BottomNavItem.Chat.icon)
+        NavigationItem(BottomNavItem.Settings.route, stringResource(id = R.string.settings), BottomNavItem.Settings.icon)
     )
     NavigationBar {
         val currentRoute = navController.currentDestination?.route
         items.forEach { item ->
             NavigationBarItem(
                 icon = { Icon(item.icon, contentDescription = item.label) },
-                label = { Text(item.label) },
+                //Modify label Sizes to fit the screen of multiple devices
+                label = {
+                    Text(
+                        item.label,
+                        fontSize = 6.sp,
+                        maxLines = 1,
+                        softWrap = false,
+                        overflow = TextOverflow.Clip
+                    )
+                },
                 selected = currentRoute == item.route,
                 onClick = {
                     navController.navigate(item.route) {
@@ -43,7 +65,8 @@ fun BottomNavigationBar(navController: NavHostController) {
                         // Avoid multiple copies of the same destination when reselecting the same item
                         launchSingleTop = true
                     }
-                }
+                },
+                
             )
         }
     }

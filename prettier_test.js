909d253c38abf6c95f3101e512f6454d9361ed93
Thalia Wood <42354895+polygeist111@users.noreply.github.com>
2025-10-30 13:12:55 -0700

    Debug Prettier pre-commit (#9)

--- prettier_test.js
+++ prettier_test.js
@@ -1,8 +1,10 @@
 const name = "James";
-const person ={first: name }
-console. log(person);
-const sayHelloLinting = (fName) => { console. log(`Hello linting, ${fName}`) }
-sayHelloLinting('James');
+const person = { first: name };
+console.log(person);
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+sayHelloLinting("James");
 // this code is not project-relevant and will be removed
 // it is simply to test that Prettier is functioning correctly as a pre-commit action
-// adding change to stage :3
+// adding change to stage :)
